46471d6f544dec562acfa11a6d8d2a646ec90cfd
vince <paulag10987@gmail.com>
2026-01-18 17:26:01 +0800

    feat(main.py): add asset comparison with conditional shading and annotations

--- week4_data_viz/main.py
+++ week4_data_viz/main.py
@@ -4,23 +4,49 @@ import numpy as np
 # Generate 100 days of data
 days = np.arange(1, 101)
 
-# start price at 100usd and then add flunctuations everyday
-price_changes = np.random.uniform(-2,1,100)
-prices = 100 + np.cumsum(price_changes)
+# Asset A (Volatile Crypto)
+# High fluctuation (-3 to +3)
+changes_a = np.random.uniform(-3, 3, 100)
+price_a = 100 + np.cumsum(changes_a)
+
+# Asset B (Stable Gold)
+# Low fluctuation (-1 to +1)
+changes_b = np.random.uniform(-1, 1, 100)
+price_b = 100 + np.cumsum(changes_b)
 
 # visualize 
-# while also setting the window size into 10 inches by 6 inches
-plt.figure(figsize=(10, 6)) 
+# while also setting the window size into 10 inches by 7 inches
+plt.figure(figsize=(12, 7)) 
 
-# plot the prices over days
-plt.plot(days, prices, color='#00ff88', linestyle='-', linewidth=2, label='Crypto-Sim Value')
+# Plot both lines
+plt.plot(days, price_a, color='#00ff88', linewidth=2, label='Crypto (Volatile)')
+plt.plot(days, price_b, color='#ffcc00', linewidth=2, linestyle='--', label='Gold (Stable)')
+
+# Fill the area between the two lines
+# "where" argument checks the logic.
+# If A > B, fill Green. If B > A, fill Red.
+plt.fill_between(days, price_a, price_b, where=(price_a > price_b),  # type: ignore
+                 interpolate=True, color='#00ff88', alpha=0.1)
+
+plt.fill_between(days, price_a, price_b, where=(price_a <= price_b),  # type: ignore
+                 interpolate=True, color='#ff0055', alpha=0.1)
+
+# 4. ANNOTATIONS (Pointing to specific events)
+# Let's point to the highest price of Crypto
+max_price = np.max(price_a)
+max_day = days[np.argmax(price_a)]
+
+plt.annotate(f'All Time High (${max_price:.2f})', 
+             xy=(max_day, max_price),            # Arrow points to this (x,y)
+             xytext=(max_day+5, max_price+5),    # Text sits here
+             arrowprops=dict(facecolor='white', shrink=0.05))
 
 # decorating
-plt.title('Market Trend Simulation (Week 4)', fontsize=16, fontweight='bold')
-plt.xlabel('Days Passed')
-plt.ylabel('Asset Price ($)')
-plt.grid(True, alpha=0.3, linestyle='--') 
-plt.legend() 
+plt.title('Asset Performance Comparison', fontsize=16, fontweight='bold')
+plt.xlabel('Days Trading')
+plt.ylabel('Price ($)')
+plt.legend()
+plt.grid(True, alpha=0.2)
 
 # Dark Mode Theme 
 plt.style.use('dark_background')
