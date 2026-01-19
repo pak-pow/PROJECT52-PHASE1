9568aa580735a54e456dbd72d1bac6a5a4e90ee4
vince <paulag10987@gmail.com>
2026-01-19 20:21:19 +0800

    feat(day2/week3/py): add comments to better understand the whole sequence

--- week4_data_viz/histogram.py
+++ week4_data_viz/histogram.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt # type: ignore
 import numpy as np
 
+# generating 1000 students exam scores
+# mean (average) = 75, standard deviation = 10
+np.random.seed(42) # ensuring we get the same "random" numbers each time we run this
 scores = np.random.normal(75,10,1000)
+
+# clipping scores so that no one gets less than 0 or more than 100
 scores = np.clip(scores,0,100)
 
+# visualize histogram
 plt.figure(figsize=(10,6))
+
+# plt.hist returns 3 values, but we only need the plot
+# bins=30 means "split the scores into 30 buckets"
+# alpha=0.7 makes the bars slightly see-through
+# color='#ff00ff' is Magenta (Cyberpunk style)
 plt.hist(scores, bins=30, color='#ff00ff', edgecolor='black', alpha=0.7)
 
 plt.axvline(60, color='red', linestyle='dashed', linewidth=2, label='Passing Grade (60)')
