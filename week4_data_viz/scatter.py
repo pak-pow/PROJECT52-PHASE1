28cf2c7998a0b64e6f8837106bb3065e0db5f269
vince <paulag10987@gmail.com>
2026-01-20 17:41:02 +0800

    chore(py): fix comments

--- week4_data_viz/scatter.py
+++ week4_data_viz/scatter.py
@@ -4,7 +4,7 @@ import numpy as np
 # generate 50 students
 np.random.seed(42)  
 
-# x axis: hours studied (random numbers between 1 and 10) 
+# X-axis: hours studied (normally distributed values, mean=1, std=10)
 study_hours = np.random.normal(1, 10, 50)
 
 # Y Axis: Test Scores
