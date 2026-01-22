d3108420c374a1c678174ce2ea80968099ae63b8
vince <paulag10987@gmail.com>
2026-01-22 16:31:35 +0800

    doc(day5): add comments for documentation and understanding of each lines

--- week4_data_viz/dashboard.py
+++ week4_data_viz/dashboard.py
@@ -20,40 +20,53 @@ popularity = [90, 85, 70, 60, 50]
 
 # Create a 2x2 grid (2 rows, 2 columns)
 # figsize=(12, 10) makes the window large enough to see everything
+# Fig: is basically the paper or window as it holds everything
+# Axs: are the stickers or the plots, it is the list (array) of the 4 graphs
 fig, axs = plt.subplots(2, 2, figsize=(12, 8))
 
 # Apply Dark Mode manually to the Figure background
 fig.patch.set_facecolor('#ffffff') # type: ignore
 plt.style.use('dark_background')
 
+# Unlike plt.plot() which just draws on the "current" active window
+# here we specify EXACTLY which square we want to paint on
+
 # TOP LEFT [0, 0]: Stock Trend
+# [0,0] means row: 0, column: 0 
 axs[0, 0].plot(days, stock_price, color='#00ff88', linewidth=2)
 axs[0, 0].set_title('Market Trend (Line)', fontsize=12, color='white')
 axs[0, 0].grid(True, alpha=0.2)
 axs[0, 0].set_facecolor('#333')
 
 # TOP RIGHT [0, 1]: Exam Distribution
+# [0,1] means row: 0, column: 1
 axs[0, 1].hist(scores, bins=20, color='#ff00ff', alpha=0.7)
 axs[0, 1].set_title('Test Scores (Histogram)', fontsize=12, color='white')
 axs[0, 1].grid(axis='y', alpha=0.2)
 axs[0, 1].set_facecolor('#333')
 
 # BOTTOM LEFT [1, 0]: Correlation
+# [1,0] means row: 1, column: 0
 axs[1, 0].scatter(study_hours, grades, color='cyan', alpha=0.8)
 axs[1, 0].set_title('Study vs Grades (Scatter)', fontsize=12, color='white')
 axs[1, 0].grid(True, alpha=0.2)
 axs[1, 0].set_facecolor('#333')
 
 # BOTTOM RIGHT [1, 1]: Tech Stack (Bar Chart)
-
+# [1,1] means row: 1, column: 1
 axs[1, 1].bar(languages, popularity, color=['#306998', '#F7DF1E', '#00599C', '#dea584', '#00ADD8'])
 axs[1, 1].set_title('Dev Language Popularity (Bar)', fontsize=12, color='white')
 axs[1, 1].set_facecolor('#333')
 
 # Super Title for the whole window
+# fig.suptitle: Sets the main title for the entire "Paper"
 fig.suptitle('DASHBOARD', fontsize=18, fontweight='bold', color='#111')
 
 # Tight Layout prevents the graphs from overlapping each other
+# plt.tight_layout(): CRITICAL COMMAND
+# Without this, the axis labels of the top row would overlap 
+# with the titles of the bottom row This function calculates
+# the perfect padding automatically
 plt.tight_layout()
 
 # Show the Masterpiece
