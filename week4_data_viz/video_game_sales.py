30e524bfdd5f76faa8a4db1b1b28bfcdb48ca3a5
vince <paulag10987@gmail.com>
2026-01-23 17:14:26 +0800

    feat(py): add os imports and add line of code that gets the dataset file and load them into the code and cleaning them

--- week4_data_viz/video_game_sales.py
+++ week4_data_viz/video_game_sales.py
@@ -1,2 +1,13 @@
 import matplotlib.pyplot as plt # type: ignore
 import pandas as pd
+import os
+
+# file paths
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "dataset", "vgsales.csv")
+
+# loading the data
+df = pd.read_csv(file_path)
+
+# droping the rows that have empty or missing info in them
+df.dropna(inplace=True)
