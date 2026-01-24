b6f0b9ed56f0eb5dcb1883282bb759c2aed6181e
vince <paulag10987@gmail.com>
2026-01-24 18:03:32 +0800

    feat(py): add chart 2 which is a pie chart

--- week4_data_viz/peak_analysis.py
+++ week4_data_viz/peak_analysis.py
@@ -23,7 +23,7 @@ top_games = df_2008.sort_values(by="Global_Sales", ascending=False).head(5)
 top_publisher = df_2008.groupby("Publisher")["Global_Sales"].sum().sort_values(ascending=False).head(5)
 
 # visualizing 
-fig, axs = plt.subplots(1,2, figsize = (14,8))
+fig, axs = plt.subplots(1,2, figsize = (16,8))
 
 axs[0].barh(
     top_games["Name"], 
@@ -33,9 +33,16 @@ axs[0].barh(
     )
 
 axs[0].invert_yaxis()
-axs[0].set_title('Top 5 Best Selling Games (2008)', fontsize=12, fontweight='bold', color='white')
+axs[0].set_title('Top 5 Best Selling Games (2008)', fontsize=12, fontweight='bold', color='black')
 axs[0].set_xlabel('Global Sales (Millions)')
 axs[0].grid(axis='x', alpha=0.2)
 
+axs[1].pie(top_publisher, labels=top_publisher.index, autopct='%1.1f%%', startangle=140, 
+           colors=['#e056fd', '#00a8ff', '#ff7979', '#f0932b', '#badc58'],
+           explode=(0.1, 0, 0, 0, 0)) # "Pull out" the biggest slice
+axs[1].set_title('Market Share by Publisher (2008)', fontsize=14, fontweight='bold', color='black')
+
+fig.suptitle('FORENSIC ANALYSIS: THE YEAR 2008', fontsize=20, color='black')
+print(f"Top Game of 2008: {top_games.iloc[0]['Name']}")
 plt.tight_layout()
 plt.show()
