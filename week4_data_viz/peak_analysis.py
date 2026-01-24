59ccb616c6af5cc61045e50183f94f7521b8204a
vince <paulag10987@gmail.com>
2026-01-24 17:52:45 +0800

    feat(py): add file path and data cleaning and filtering

--- week4_data_viz/peak_analysis.py
+++ week4_data_viz/peak_analysis.py
@@ -2,3 +2,14 @@ import matplotlib.pyplot as plt # type: ignore
 import pandas as pd
 import os
 
+# load file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "dataset", "vgsales.csv")
+df = pd.read_csv(file_path)
+
+# data cleaning
+df.dropna(inplace=True)
+df['Year'] = df['Year'].astype(int)
+
+# data filtering
+df_2008 = df[df['Year'] == 2008]
