a478887d735215980b61e9db6911797bb680cc7c
vince <paulag10987@gmail.com>
2026-01-26 16:54:24 +0800

    feat(js): add constant 4 questions

---
+++ week5_interactive_quiz/script.js
@@ -0,0 +1,25 @@
+const questions = [
+    {
+        question:"Which language runs in a web browser?",
+        options: ["Java", "C", "Python", "JavaScript"],
+        correct: "JavaScript"
+    },
+
+    {
+        question: "What does CSS stand for?",
+        options: ["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        correct: "Cascading Style Sheets"
+    },
+    
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "None of the above"],
+        correct: "1995"
+    },
+    
+    {
+        question: "Which HTML tag is used for JavaScript?",
+        options: ["<js>", "<script>", "<javascript>", "<code >"],
+        correct: "<script>"
+    }
+];
