615125abcf090de645695fc44afab761c2ecc352
vince <paulag10987@gmail.com>
2026-01-26 17:10:05 +0800

    feat(js): add button functionality and importing questions

--- week5_interactive_quiz/script.js
+++ week5_interactive_quiz/script.js
@@ -1,3 +1,4 @@
+// array of questions
 const questions = [
     {
         question:"Which language runs in a web browser?",
@@ -23,3 +24,42 @@ const questions = [
         correct: "<script>"
     }
 ];
+
+// current states
+let currentQuestionIndex = 0;
+let score = 0;
+
+// dom selector
+const questionElement = document.getElementById('question-text');
+const scoreElement = document.getElementById('score');
+
+// selecting all buttons inside the options container
+const optionButtons = document.querySelectorAll('.options-container .btn');
+const nextButton = document.getElementById('next-btn');
+
+// functions
+function loadQuestion(){
+
+    // getting the data from the current question
+    const currentQuestionData = questions[currentQuestionIndex];
+
+    // updating the quesion text
+    questionElement.innerText = currentQuestionData.question;
+
+    // updating the option buttons 
+    // looping through the 4 buttons and text from the data
+    optionButtons.forEach((button, index) => {
+        button.innerText = currentQuestionData.options[index];
+        button.classList.remove('correct','wrong');
+        button.disabled = false;
+    })
+
+    // hide the next button until it is answered
+    nextButton.classList.add('hide')
+}
+
+function checkAnswer(selectedButton) {
+    console.log("Button clicked:", selectedButton.innerText);
+}
+
+loadQuestion()
