9f71c11213a1dff84aa99a0ee59428a1f8563efa
vince <paulag10987@gmail.com>
2026-02-01 11:34:59 +0800

    feat(scrapper): enhance output with raw HTML preview after status code

--- week6_web_scrapper/scrapper.py
+++ week6_web_scrapper/scrapper.py
@@ -12,3 +12,10 @@ response = requests.get(url)
 # 404 not found
 # 403 forbidden, it means they blocked our bot
 print(f"Status Code: {response.status_code}")
+
+# "response.text" is the raw HTML code of the website.
+# We will print just the first 500 characters to verify it worked.
+print("\nRAW HTML PREVIEW:")
+print("--------------------------------------------------")
+print(response.text[:500])  # Slicing the string to show only start
+print("--------------------------------------------------")
