78ad7e96d29f5bce8d4851030ef31c86632d084c
vince <paulag10987@gmail.com>
2026-02-01 11:34:18 +0800

    feat(scrapper): add status code printout for web scraping response

--- week6_web_scrapper/scrapper.py
+++ week6_web_scrapper/scrapper.py
@@ -7,3 +7,8 @@ url = "https://news.ycombinator.com/"
 # getting the request
 print(f"Connecting to {url}...")
 response = requests.get(url)
+
+# 200 ok
+# 404 not found
+# 403 forbidden, it means they blocked our bot
+print(f"Status Code: {response.status_code}")
