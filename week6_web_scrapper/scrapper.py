b0651395ed4c81a975f90db3303b8c9223d7296b
vince <paulag10987@gmail.com>
2026-02-01 11:33:17 +0800

    feat(scrapper): add initial web scraping setup for Hacker News

--- week6_web_scrapper/scrapper.py
+++ week6_web_scrapper/scrapper.py
@@ -1,2 +1,9 @@
 import requests
 
+# our targer 
+# we are going to be scrapping the hacker news it is a simple text-based tech news site
+url = "https://news.ycombinator.com/"
+
+# getting the request
+print(f"Connecting to {url}...")
+response = requests.get(url)
