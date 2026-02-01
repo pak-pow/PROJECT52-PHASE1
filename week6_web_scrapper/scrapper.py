3985041ef43697e30605e0196a10918869dee3d1
vince <paulag10987@gmail.com>
2026-02-01 11:39:32 +0800

    feat(scrapper): implement web scraping logic to extract headlines from Hacker News

--- week6_web_scrapper/scrapper.py
+++ week6_web_scrapper/scrapper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 # our targer 
 # we are going to be scrapping the hacker news it is a simple text-based tech news site
@@ -8,14 +9,39 @@ url = "https://news.ycombinator.com/"
 print(f"Connecting to {url}...")
 response = requests.get(url)
 
+if response.status_code == 200:
+    
+    # soup 
+    # this turns the raw text into a navigatable tree
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # searching
+    # looking for a span tags that have a class titleline
+    headlines = soup.select("span.titleline")
+    
+    # looping
+    # iterating through the list of headlines we found
+    print(f"\nFound {len(headlines)} Headlines:\n")
+    
+    for i, headline in enumerate(headlines, 1):
+        # The actual text is inside an <a> tag inside the <span>
+        text = headline.find("a").get_text()
+        print(f"{i}. {text}")
+
+else:
+    print("Failed to Connect")
+
+
+
+# ============================================================
 # 200 ok
 # 404 not found
 # 403 forbidden, it means they blocked our bot
-print(f"Status Code: {response.status_code}")
+#print(f"Status Code: {response.status_code}")
 
 # "response.text" is the raw HTML code of the website.
 # We will print just the first 500 characters to verify it worked.
-print("\nRAW HTML PREVIEW:")
-print("--------------------------------------------------")
-print(response.text[:500])  # Slicing the string to show only start
-print("--------------------------------------------------")
+#print("\nRAW HTML PREVIEW:")
+#print("--------------------------------------------------")
+#print(response.text[:500])  # Slicing the string to show only start
+#print("--------------------------------------------------")
