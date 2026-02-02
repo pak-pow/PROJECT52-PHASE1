573306f405f91a0219290fe6ffb149f5877532e3
vince <paulag10987@gmail.com>
2026-02-02 13:41:47 +0800

    feat(scrapper): add connection handling and response status check for book scraping

--- week6_web_scrapper/book_scrapper.py
+++ week6_web_scrapper/book_scrapper.py
@@ -3,3 +3,11 @@ import csv
 from bs4 import BeautifulSoup
 
 url = "https://books.toscrape.com"
+print(f"Connecting to {url}...")
+response = requests.get(url)
+
+if response.status_code == 200:
+    print(f"Connection Succesfull Parsing Books")
+
+else:
+    print("Failed to connect")
