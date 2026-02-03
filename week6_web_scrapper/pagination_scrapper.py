46fd1851f4021145d9c8069cc064e7608e616862
vince <paulag10987@gmail.com>
2026-02-03 11:28:08 +0800

    feat(scrapper): initialize pagination scrapper with base URL and setup for book extraction

--- week6_web_scrapper/pagination_scrapper.py
+++ week6_web_scrapper/pagination_scrapper.py
@@ -3,3 +3,16 @@ import csv
 import requests
 import time
 from urllib.parse import urljoin
+
+base_url = "https://books.toscrape.com"
+current_url = base_url
+all_books = []
+
+print("Starting scrapping...")
+
+while True:
+    print(f"Scrapping: {base_url}")
+    respone = requests.get(base_url)
+    soup = BeautifulSoup(respone.text, "html.parser")
+    
+    books = soup.select("article.product_pod")
