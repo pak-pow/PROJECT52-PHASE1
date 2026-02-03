91da4d58bb19dbe5939d6d4f4a877f0889e2342a
vince <paulag10987@gmail.com>
2026-02-03 11:32:13 +0800

    feat(scrapper): add detailed book extraction including title, price, stock, and rating

--- week6_web_scrapper/pagination_scrapper.py
+++ week6_web_scrapper/pagination_scrapper.py
@@ -16,3 +16,16 @@ while True:
     soup = BeautifulSoup(respone.text, "html.parser")
     
     books = soup.select("article.product_pod")
+    
+    for book in books:
+        title = book.h3.find("a")['title']
+        raw_price = book.select_one(".price_color").text
+        price_text = raw_price.replace('£', '')
+        price_text2 = price_text.replace('Â', '')
+        price = float(price_text2)
+        stock = book.select_one(".instock.availability").text.strip()
+        rating = book.select_one(".star-rating")["class"][1]
+        
+        all_books.append([title, price, stock, rating])
+
+        
