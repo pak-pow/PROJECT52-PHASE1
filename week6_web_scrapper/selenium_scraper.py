549a7675c74fd76bf0eb85f53158d3b98857a5da
vince <paulag10987@gmail.com>
2026-02-04 20:11:49 +0800

    feat(py): add bot to navigate the website and time delay to set the data to load in

--- week6_web_scrapper/selenium_scraper.py
+++ week6_web_scrapper/selenium_scraper.py
@@ -1,4 +1,13 @@
-from selenium import webdriver # type: ignore
-from selenium.webdriver.common.by import By # type: ignore
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
 import time
 
+print("Lunching chrome bot")
+driver = webdriver.Chrome()
+
+url = "https://quotes.toscrape.com/js/"
+print(f"Navigating to {url}")
+driver.get(url)
+
+print("Wating for JS to load...")
+time.sleep(3)
