d840bc8ca699897d558f18bc4bb6873e4a2e6cf2
vince <paulag10987@gmail.com>
2026-02-04 20:26:31 +0800

    feat(py): add bot logic to print out all quotes with author

--- week6_web_scrapper/selenium_scraper.py
+++ week6_web_scrapper/selenium_scraper.py
@@ -11,3 +11,16 @@ driver.get(url)
 
 print("Wating for JS to load...")
 time.sleep(3)
+
+quotes = driver.find_elements(By.CLASS_NAME, "quote")
+print(f"found {len(quotes)} Quotes: \n")
+
+for quote in quotes:
+    text = quote.find_element(By.CLASS_NAME, "text").text
+    author = quote.find_element(By.CLASS_NAME, "author").text
+    
+    print(f" {text}")
+    print(f"   — {author}")
+    print("-" * 20)
+    
+driver.quit()
