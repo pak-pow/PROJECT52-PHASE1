500366f28ac7ef967b23ce0e57abce417fd39cf7
vince <paulag10987@gmail.com>
2026-02-05 22:10:21 +0800

    feat(py): add bot login and password

--- week6_web_scrapper/login_bot.py
+++ week6_web_scrapper/login_bot.py
@@ -7,3 +7,17 @@ driver = webdriver.Chrome()
 url = "https://quotes.toscrape.com/login"
 print(f"Connecting to {url}")
 driver.get(url)
+
+username_input = driver.find_element(By.ID, "username")
+password_input = driver.find_element(By.ID, "password")
+
+print("typing credentials..")
+username_input.send_keys("my_secret_bot")
+time.sleep(2)
+
+password_input.send_keys("password123")
+time.sleep(2)
+
+print("Logging in the web..")
+password_input.send_keys(Keys.RETURN)
+
