ad2ad0619e469bdb5ff483b3fe18386514e12a17
vince <paulag10987@gmail.com>
2026-02-06 20:25:00 +0800

    feat(py): add chrome options for setting up ghost bot

--- week6_web_scrapper/headless_job_bot.py
+++ week6_web_scrapper/headless_job_bot.py
@@ -3,3 +3,10 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--log-level=3")
+
+print("Initializing ghost bot without the window.")
+driver = webdriver.Chrome(options=chrome_options)
