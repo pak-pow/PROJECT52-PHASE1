626e31819e40d7149879605aee626ed9b941e520
vince <paulag10987@gmail.com>
2026-02-15 18:14:13 +0800

    chore(py): add boilerplate pygame main class

--- week8_algo_viz/main.py
+++ week8_algo_viz/main.py
@@ -2,3 +2,13 @@ import pygame #type: ignore
 import math
 import random
 
+class Main:
+    def  __init__(self) -> None:
+        pass
+    
+    def run(self):
+        pass
+    
+if __name__ == "__main__":
+    app = Main()
+    app.run()
