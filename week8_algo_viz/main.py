00347f2f7e27727a44c0242a3e222b2dbccc53d2
vince <paulag10987@gmail.com>
2026-02-16 21:48:24 +0800

    chore(py): add display width and height and also colors

--- week8_algo_viz/main.py
+++ week8_algo_viz/main.py
@@ -3,8 +3,22 @@ import math
 import random
 
 class Main:
-    def  __init__(self) -> None:
-        pass
+    
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    GREY = (128, 128, 128)
+    BACKGROUND_COLOR = BLACK
+
+    def  __init__(self):
+        pygame.init()
+        
+        self.DISPLAY_WIDTH = 800
+        self.DISPLAY_HEIGHT = 600
+        self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH,self.DISPLAY_HEIGHT))
+        
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
     
     def run(self):
         pass
