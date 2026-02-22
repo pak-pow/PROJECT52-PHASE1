8c38a8a0d4d7b024fa07065745c3dfd59cf62de5
vince <paulag10987@gmail.com>
2026-02-22 18:14:30 +0800

    chore(js): add event listener to the form

--- week9_form_validator/validator.js
+++ week9_form_validator/validator.js
@@ -1,7 +1,19 @@
 class Validator {
     constructor(formId){
-        this.form = document.getElementById(formId)
+        this.form = document.getElementById(formId);
 
+        if (!this.form){
+            console.error(`Validator Error: Form with ID '${formId}' not found.`);
+            return;
+        }
+
+        this.form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            this.validateForm();
+        });
+    }
+
+    validateForm(){
         
     }
 }
