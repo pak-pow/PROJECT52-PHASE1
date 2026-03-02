896839497ec8eb4a7357c0748d8c5ce9277f2824
vince <paulag10987@gmail.com>
2026-03-02 19:40:37 +0800

    chore(py): add total revenue using the sum function of python and printing it out

--- week10_panda_data_analysis/main.py
+++ week10_panda_data_analysis/main.py
@@ -5,7 +5,18 @@ def load_data():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     csv_path = os.path.join(current_dir, "app_data.csv")
     
+    # returns the dataframe so other functions can use it
     return pd.read_csv(csv_path)
     
+    
+def print_statistic(df):
+    print("CORE STATISTIC")
+    print("-" * 40)
+    
+    # calculating total revenue
+    total_revenue = df['Revenue_USD'].sum()
+    print(f"Total Revenue: ${total_revenue:,.2f}")
+
 if __name__ == "__main__":
-    load_data()
+    app_df = load_data()
+    print_statistic(app_df)
