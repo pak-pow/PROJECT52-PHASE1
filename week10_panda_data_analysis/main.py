209eb54f28604bd747e2706beadf292c460b8692
vince <paulag10987@gmail.com>
2026-03-03 20:41:53 +0800

    chore(py): add best revenue of that day and added new comments in understanding the code

--- week10_panda_data_analysis/main.py
+++ week10_panda_data_analysis/main.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 def load_data():
+    # The code snippet `current_dir = os.path.dirname(os.path.abspath(__file))` is getting the
+    # directory path of the current Python script file. It uses `os.path.abspath(__file__)` to get the
+    # absolute path of the current script file and then `os.path.dirname()` to extract the directory
+    # path from the absolute path.
     current_dir = os.path.dirname(os.path.abspath(__file__))
     csv_path = os.path.join(current_dir, "app_data.csv")
     
@@ -35,12 +39,23 @@ def filter_data(df):
     print("[DATA FILTERING]: ")
     print("-" * 40)
     
+    # This code snippet is filtering the DataFrame `df` to find and display the rows where the
+    # 'Crash_Reports' column has a value greater than 10.
     high_crash_days = df[df['Crash_Reports'] > 10]
     print("Days with > 10 crashes")
     print(high_crash_days[['Date', 'Crash_Reports']])
     print("-" * 40)
     
+    # This line of code is filtering the DataFrame `df` to find the row(s) where the 'Revenue_USD'
+    # column has the maximum value.
+    best_revenue_day = df[df['Revenue_USD'] == df['Revenue_USD'].max()]
+    print("Best Revenue Day:")
+    print(best_revenue_day[['Date', 'Revenue_USD']])
+    print("-" * 40)
+    
 
 if __name__ == "__main__":
+    # The code snippet `app_df = load_data()` loads data from a CSV file into a pandas DataFrame and
+    # assigns it to the variable `app_df`.
     app_df = load_data()
     filter_data(app_df)
