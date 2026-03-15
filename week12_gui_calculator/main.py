e8ee33bb18b4d61607530c475a36ef286e63f70b
vince <paulag10987@gmail.com>
2026-03-15 16:35:30 +0800

    feat(py/gui_calculator): add base class file oop for the week12 project

---
+++ week12_gui_calculator/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+class Calculator():
+    def __init__(self, root):
+        pass
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Calculator(root)
+    root.mainloop
