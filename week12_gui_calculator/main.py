c4c37b027feac938e705ee959993351c96a33126
vince <paulag10987@gmail.com>
2026-03-16 15:31:51 +0800

    chore(comment): add doc in each line of code

--- week12_gui_calculator/main.py
+++ week12_gui_calculator/main.py
@@ -8,6 +8,12 @@ class Calculator:
         self.root.resizable(False, False)
         self.root.configure(bg="#f4f7f6") 
         
+        """
+        `self.display = tk.Entry(` is creating an Entry widget in the tkinter GUI application. An
+        Entry widget is used to display a single-line text field where the user can enter or display
+        text. In this case, it is being used as a display for the calculator where the user can see
+        the input and output of the calculations.
+        """
         self.display = tk.Entry(
             self.root, 
             font=("Segeo UI", 32, "bold"),
@@ -17,6 +23,11 @@ class Calculator:
             justify="right"
         )
         
+        """
+        `self.display.pack(` is configuring the layout of the Entry widget (`self.display`) within
+        the tkinter GUI application. The `pack` method is used to organize widgets in blocks before
+        placing them in the parent widget.
+        """
         self.display.pack(
             expand=False, 
             fill="both", 
@@ -25,6 +36,12 @@ class Calculator:
             padx=20
         )
         
+        """
+        `self.button_frame = tk.Frame(` is creating a Frame widget in the tkinter GUI application. A
+        Frame widget is a container that holds other widgets and helps in organizing the layout of
+        the GUI components. In this case, `self.button_frame` is being used to group and organize
+        the buttons that will be part of the calculator interface.
+        """
         self.button_frame = tk.Frame(
             self.root,
             bg="#f4f7f6"
